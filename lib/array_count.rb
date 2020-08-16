383ebbd0d109a9cf7282b26648596bc174c3d19f
mamadou alpha barry <mamadou.barry1105@gmail.com>
2020-08-16 19:42:13 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,5 +4,6 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  # Return the total number of EMPTY strings in the provided array using the count 
+  array.count { |i| i == ""} 
 end
